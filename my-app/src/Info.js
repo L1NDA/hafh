5890f86d6a39fd09f118a1a99fb9a070c0a4c4ee
Waverley He <waverleyhe@Waverleys-MacBook-Pro.local>
2018-04-10 20:04:06 -0400

    housing info updatedd

--- my-app/src/Info.js
+++ my-app/src/Info.js
@@ -26,24 +26,25 @@ class Info extends Component {
     return (
           <div className="info-box">
               <div className="title">What to Expect When Looking for Housing</div>
-              <form className="body">
-                  Several factors are typically considered when people look for housing, including <b>cost</b> and <b>location</b>.<br/>
-                  <br/>
-                  The average cost to rent a <input type="text" className="bedroom" placeholder="1"></input>-bedroom
-                  , <input type="text" className="bathroom" placeholder="1"></input>-bathroom listing in Boston is <b>$___</b> per month.
-                  You may find that the most affordable option is to share your living space or rent out of a larger home.<br/>
-                  <br/>
+              <div className="body">
+                  Several factors are typically considered when people look for housing, including <b>cost</b> and <b>location</b>.
+                  <br/><br/>
+                  <form>
+                      The average cost to rent a <input type="text" placeholder="1"></input>-bedroom
+                      , <input type="text" placeholder="1"></input>-bathroom listing in Boston is <b>$___</b> per month.
+                      You may find that the most affordable option is to share your living space or rent out of a larger home.
+                  </form><br/>
                   Immigrants in the <b>Chinese</b> community frequently choose to live in <b>Quincy, Malden, Acton, Newton, and Lexington</b>.
+                  <br/><br/>
                   <Iframe
-                      url="https://www.google.com/maps/embed/v1/place?key=AIzaSyAH5uMfQyfWXcmc9WW41XNKdRMyTuzsuHk
-    &q=Space+Needle,Seattle+WA"
+                      url="https://www.google.com/maps/d/embed?mid=1_XFwbb5WOTNpAU_zk_unEkuUM9nOLL8T"
                       width="450"
                       height="450"
                       frameborder="0" style="border:0"
                       display="initial"
                       position="relative"
                       allowfullscreen/>
-              </form>
+              </div>
           </div>
     )
   }
