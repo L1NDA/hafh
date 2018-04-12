250d5fd609ba3ebcd0a7d34f05d53a2f352b8312
Avinash Lal <lal@college.harvard.edu>
2018-04-11 21:27:04 -0400

    idk why it doesnt woimagebox doesnt work

--- my-app/src/Categories.js
+++ my-app/src/Categories.js
@@ -12,15 +12,27 @@ import {
 import FontAwesome from "react-fontawesome";
 import Select from 'react-select';
 import 'react-select/dist/react-select.css';
-import Info2 from './Info2.js';
+//import Info2 from './Info2.js';
+import ImageBox from "./ImageBox.js";
 
 class Categories extends Component {
   constructor (props) {
     super(props);
     this.state = {
-
+       CategoryName: ["HW"],
+       GridImg: ["./img/stock2.jpg"]
     }
   }
+  createPost = () => {
+    var posts = []
+    for (var i = 0; i < this.state.counter; i++) {
+      posts.push(<ImageBox GridImg={this.state.GridImg[i]} CategoryName = {this.state.CategoryName[i]} />)
+    }
+    return posts
+  }
+
+  
+
 
   render () {
 
@@ -30,7 +42,8 @@ class Categories extends Component {
           <div className="categories">
             <Header button1="PROFILE" button2="LOG OUT" link="https://l1nda.github.io/hafh/#/"/>
             <div className="categories-box">
-              <Info2 category="housing"/>
+              {/* <Info2 category="housing"/> */}
+              {this.createPost()}
             </div>
           </div>
         </switch>
