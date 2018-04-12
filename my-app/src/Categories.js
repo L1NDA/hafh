9d415da8b7f8ed832053984fb5f22f2105dd13a2
chrisrob <rashawn302@yahoo.com>
2018-04-11 20:17:01 -0400

    food

--- my-app/src/Categories.js
+++ my-app/src/Categories.js
@@ -2,32 +2,24 @@ import React, { Component } from 'react';
 import classNames from "classnames";
 import "./css/categories.css";
 import Header from './Header.js';
-import ImageBox from './ImageBox.js'
 import { HashRouter } from 'react-router-dom';
 import {
   BrowserRouter as Router,
   Route,
   Link
 } from 'react-router-dom';
+
 import FontAwesome from "react-fontawesome";
 import Select from 'react-select';
 import 'react-select/dist/react-select.css';
+import Info2 from './Info2.js';
 
 class Categories extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      GridImage: ["./img/stock.jpeg"],
-      CategoryName: ["Home"],
-    }
-  }
 
-  createImage = () => {
-    var Box = []
-    for (var i = 0; i < this.state.counter; i++) {
-      Box.push(<ImageBox GridImage={this.state.query[i]} CategoryName={this.state.CategoryName}/>)
     }
-    return Box
   }
 
   render () {
@@ -37,9 +29,9 @@ class Categories extends Component {
         <switch>
           <div className="categories">
             <Header button1="PROFILE" button2="LOG OUT" link="https://l1nda.github.io/hafh/#/"/>
-            <div className="categories-box"> 
-            </div>           
-            {this.createImage()}
+            <div className="categories-box">
+              <Info2 category="housing"/>
+            </div>
           </div>
         </switch>
       </HashRouter>
