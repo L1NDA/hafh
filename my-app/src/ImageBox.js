33b145e0a1fc6547d50831f4d59e02c97782b569
Avinash Lal <lal@college.harvard.edu>
2018-04-11 22:12:49 -0400

    didnt add imgbox component

---
+++ my-app/src/ImageBox.js
@@ -0,0 +1,37 @@
+import React, { Component }  from 'react';
+import ReactDOM from 'react-dom'
+import './App.css';
+import ImgRoot from "./img/heart.png";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from 'react-router-dom'
+import "./css/react-grid-layout-styles.css";
+import "./css/react-resizable-styles.css";
+import "./css/categories.css";
+import ScrollManager from 'window-scroll-manager'
+
+class ImageBox extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+    
+    render() {
+
+    return (
+      <div className="ImageBox">
+       <div className="CategoryName">
+            {this.props.CategoryName}
+       </div>
+       <img src= {require(`${this.props.GridImg}`)} className="category-pic" />
+      </div>
+    );
+  }
+}
+
+export default ImageBox;
